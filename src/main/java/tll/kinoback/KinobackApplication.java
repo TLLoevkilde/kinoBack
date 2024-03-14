80fc57c8aecf1bf2c98d85c0b3fe6f3e0f621903
Thomas <tlloevkilde@gmail.com>
2024-03-14 11:55:16 +0100

    Initial Commit

---
+++ src/main/java/tll/kinoback/KinobackApplication.java
@@ -0,0 +1,13 @@
+package tll.kinoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tll/kinoback/KinobackApplicationTests.java
@@ -0,0 +1,13 @@
+package tll.kinoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
